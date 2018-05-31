552bc0d2c9f9feda85f77d83e326124659fddd90
wbyh <514778666@qq.com>
2018-05-31 23:59:23 +0800

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -14,6 +14,15 @@ def index(request):
 
 def sss(request):
     conn = sqlite3.connect('test.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE COMPANY
+           (ID INT PRIMARY KEY     NOT NULL,
+           NAME           TEXT    NOT NULL,
+           AGE            INT     NOT NULL,
+           ADDRESS        CHAR(50),
+           SALARY         REAL);''')
+    conn.commit()
+    conn.close()
     return HttpResponse('Hello from Python!');
 
 

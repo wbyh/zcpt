f533e6344cb954e26ce16a35836316364baa2329
wbyh <514778666@qq.com>
2018-06-01 04:45:18 +0800

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -17,12 +17,9 @@ def aaa(request):
     c = conn.cursor()
     c.execute('''CREATE TABLE COMPANY
            (ID INT PRIMARY KEY     NOT NULL,
-           NAME           TEXT    NOT NULL,
-           AGE            INT     NOT NULL,
-           ADDRESS        CHAR(50),
-           SALARY         REAL);''')
-    c.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-      VALUES (1, 'Paul', 32, 'California', 20000.00 )");
+           NAME           INT    NOT NULL,
+           AGE            INT     NOT NULL);''')
+    c.execute("INSERT INTO COMPANY (ID,NAME,AGE) VALUES (1, 1, 32 )");
     conn.commit()
     conn.close()
     return HttpResponse('Hello from Python!');

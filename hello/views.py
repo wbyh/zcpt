80c85e18be636c1d3298dcc76485318fe19a3f09
wbyh <514778666@qq.com>
2018-06-01 00:13:10 +0800

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -28,9 +28,12 @@ def aaa(request):
 def sss(request):
     conn = sqlite3.connect('test.db')
     c = conn.cursor()
-    c.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
-          VALUES (1, 'Paul', 32, 'California', 20000.00 )");
-    conn.commit()
+    cursor = c.execute("SELECT id, name, address, salary  from COMPANY")
+    for row in cursor:
+       print "ID = ", row[0]
+       print "NAME = ", row[1]
+       print "ADDRESS = ", row[2]
+       print "SALARY = ", row[3], "\n"
     conn.close()
     return HttpResponse('Hello from Python!');
 

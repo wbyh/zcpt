be6ad37ef41e055649f7a6027364cf509bfb78fd
wbyh <514778666@qq.com>
2018-06-01 04:27:02 +0800

    Update settings.py

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.csrf.CsrfResponseMiddleware',
 ]
 
 ROOT_URLCONF = 'gettingstarted.urls'

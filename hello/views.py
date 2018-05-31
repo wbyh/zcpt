20223ee7d0e3bf7bed4bfb09275fb2b0cdc28c97
wbyh <514778666@qq.com>
2018-06-01 00:09:02 +0800

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -15,6 +15,21 @@ def index(request):
 def sss(request):
     conn = sqlite3.connect('test.db')
     c = conn.cursor()
+    c.execute('''CREATE TABLE COMPANY
+           (ID INT PRIMARY KEY     NOT NULL,
+           NAME           TEXT    NOT NULL,
+           AGE            INT     NOT NULL,
+           ADDRESS        CHAR(50),
+           SALARY         REAL);''')
+    c.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+          VALUES (1, 'Paul', 32, 'California', 20000.00 )");
+    conn.commit()
+    conn.close()
+    return HttpResponse('Hello from Python!');
+
+def sss(request):
+    conn = sqlite3.connect('test.db')
+    c = conn.cursor()
     c.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
           VALUES (1, 'Paul', 32, 'California', 20000.00 )");
     conn.commit()
